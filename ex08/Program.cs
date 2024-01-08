5eeb1813bc607eaadbf0f4af148ddaf65c78ce80
etsuee <148324929+etsuee@users.noreply.github.com>
2024-01-08 17:42:32 +0100

    Add files via upload
    ajout ex08 ettsouli

---
+++ ex08/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ft_print_comb();
+    }
+
+    void ft_print_comb()
+    {
+        for (int i = 0; i <= 7; i++)
+        {
+            for (int j = i + 1; j <= 8; j++)
+            {
+                for (int k = j + 1; k <= 9; k++)
+                {
+                    Console.Write($"{i}{j}{k}");
+
+                    if (!(i == 7 && j == 8 && k == 9))
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
