1892edb8bfe7c4bdffa924adf963888a4c8d9e42
etsuee <148324929+etsuee@users.noreply.github.com>
2024-01-08 17:42:15 +0100

    Add files via upload
    ajout ex07 ettsouli

---
+++ ex07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ft_is_negative(-5);
+        ft_is_negative(0);
+        ft_is_negative(10);
+    }
+
+    void ft_is_negative(int n)
+    {
+        if (n < 0)
+        {
+            Console.WriteLine('N');
+        }
+        else
+        {
+            Console.WriteLine('P');
+        }
+    }
+}
