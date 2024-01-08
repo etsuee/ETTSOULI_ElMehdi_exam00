f161a9824df24f9b14170a0e81ecec26549355eb
etsuee <148324929+etsuee@users.noreply.github.com>
2024-01-08 17:42:01 +0100

    Add files via upload
    ajout ex06 ettsouli

---
+++ ex06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ft_print_reverse_alphabet();
+    }
+
+    static void ft_print_reverse_alphabet()
+    {
+        char letter = 'z';
+
+        while (letter >= 'a')
+        {
+            Console.Write(letter);
+            lettre--;
+        }
+
+        Console.WriteLine();
+    }
+}
