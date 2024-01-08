0ed2cfb4ce0dd1d4eaec79b790f7d15ede1ca04b
etsuee <148324929+etsuee@users.noreply.github.com>
2024-01-08 17:41:45 +0100

    Add files via upload
    ajout ex05 ettsouli

---
+++ ex05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+    {
+        static void Main()
+        {
+            ft_print_alphabet();
+        }
+
+        static void ft_print_alphabet()
+        {
+            char lettre = 'a';
+
+            while (lettre <= 'z')
+            {
+                Console.Write(lettre);
+                lettre++;
+            }
+
+            Console.WriteLine();
+        }
+    }
